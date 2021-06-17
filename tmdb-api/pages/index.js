4d9419577958510549bead1c701171f56b05e1e4
ashur-k <ashurkanwal@yahoo.com>
2021-06-17 15:02:44 +0100

    sorted by rating functionality implmented

--- tmdb-api/pages/index.js
+++ tmdb-api/pages/index.js
@@ -15,7 +15,7 @@ export default function Home({articles:{results}}) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch (`https://api.themoviedb.org/3/discover/movie?with_genres=Comedy&with_cast=23659&sort_by=revenue.desc&api_key=8af03f276cc7c85ac2ecce75b2cc2d57&language=eng`)
+  const res = await fetch (`https://api.themoviedb.org/3/discover/movie?with_genres=Comedy&with_cast=23659&sort_by=vote_average.desc&api_key=8af03f276cc7c85ac2ecce75b2cc2d57&language=eng`)
   const articles = await res.json()
 
   return {
