fe47b27969a5d3fa48781d9f098e7b430e028a6c
ashur-k <ashurkanwal@yahoo.com>
2021-06-17 12:35:13 +0100

    dispalying comedy movies

--- tmdb-api/pages/index.js
+++ tmdb-api/pages/index.js
@@ -2,17 +2,23 @@ import Head from 'next/head'
 
 
 export default function Home({articles}) {
-  console.log('assfsdaf', articles)
+  //console.log('assfsdaf', articles)
+  const results = articles.results;
+  console.log(results)
   return (
     <div>
         <h1>Welcom to the Next</h1>
+        {
+          results.map((article) =>(
+                <h3>{article.original_title}</h3>
+          ))}
 
     </div>
   )
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch (`https://api.themoviedb.org/3/movie/76341?api_key=8af03f276cc7c85ac2ecce75b2cc2d57&language=eng`)
+  const res = await fetch (`https://api.themoviedb.org/3/discover/movie?with_genres=Comedy&with_cast=23659&sort_by=revenue.desc&api_key=8af03f276cc7c85ac2ecce75b2cc2d57&language=eng`)
   const articles = await res.json()
 
   return {
